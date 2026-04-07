5ae795299671ab8944cf75fbc7b86f26a2b6f9db
Saliou2003 <serignesalioumboup956@gmail.com>
2026-04-07 13:03:31 +0000

    Implementation des fichiers pour docker

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,5 +10,30 @@ class ExampleTest extends TestCase
     {
         // Solution simple : toujours passer le test
         $this->assertTrue(true);
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ExampleTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    public function test_homepage_redirects_to_tasks(): void
+    {
+        $response = $this->get('/');
+        $response->assertStatus(302);
+        $response->assertRedirect('/tasks');
+    }
+
+    public function test_tasks_page_returns_successful_response(): void
+    {
+        $response = $this->get('/tasks');
+        $response->assertStatus(200);
+    }
+
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get('/tasks');
+        $response->assertStatus(200);
+    }
+}
     }
 }
